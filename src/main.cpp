e8331b59ad55311bca49f1e9117663887679be4b
Happy Coder <suangshuo_ok@outlook.com>
2023-12-12 13:10:53 +0800

    提交所需文件夹

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > 程序名: main.cpp
+    > 作者: suangshuo
+    > Mail: suangshuo@yeah.net 
+    > 创建时间: 2023年12月12日 13:01:27
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+auto main()->int{
+    cout<<"Hello,world!"<<endl;
+
+   return 0;
+}
